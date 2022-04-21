1066687b9c04b02d91820eb4983ddaa0987de2c8
Tam Tran <trantamjp@gmail.com>
2022-04-20 17:33:33 -0700

    Added denvertion built on debian10-slim

---
+++ redpill-virtio/src/check-virtio.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+echo "Checking for VirtIO"
+if (grep -r -q -E "(QEMU|VirtualBox)" /sys/devices/virtual/dmi/id/); then
+  echo "VirtIO hypervisor detected"
+  exit 0
+else
+  echo "*No* VirtIO hypervisor detected"
+  exit 1
+fi
